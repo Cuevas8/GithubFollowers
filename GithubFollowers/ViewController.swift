0b1566d4a5a3902bacf9f9665f7d3896c600307c
Bryan Cuevas <bcuevas8@csu.fullerton.edu>
2020-04-11 17:12:58 -0700

    Add files via upload

---
+++ GithubFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubFollowers
+//
+//  Created by Bryan Cuevas on 4/11/20.
+//  Copyright © 2020 bryanCuevas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
